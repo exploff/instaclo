d4dc0e7ac56bec7fbf9ad0863e326d4aaf5d85a8
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2022-10-21 09:01:37 +0200

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'guest', loadChildren: () => import('./modules/guest/guest/guest.module').then(m => m.GuestModule) }, { path: 'guest', loadChildren: () => import('./modules/guest/guest.module').then(m => m.GuestModule) }, { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
