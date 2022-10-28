51514628a724edfedb2350ddcddad60ee7bbe957
Julien Bedia <bedia.pro@gmail.com>
2022-10-28 10:31:17 +0200

    Authentificatino service : signup signin

--- src/app/core/services/authentification/authentification.service.ts
+++ src/app/core/services/authentification/authentification.service.ts
@@ -1,54 +1,70 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import { FirebaseError } from "@angular/fire/app";
-import { Auth, authState, signInWithEmailAndPassword, createUserWithEmailAndPassword, User, UserCredential, onAuthStateChanged, updateProfile, signOut } from "@angular/fire/auth";
+import {
+  Auth,
+  authState,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  createUserWithEmailAndPassword,
+  User,
+  UserCredential,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+  GoogleAuthProvider,
+  signInWithPopup
+} from "@angular/fire/auth";
 import { EMPTY, Observable, of } from "rxjs";
+import { UserService } from "../user/user.service";
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+
   public user: Observable<User | null> = EMPTY;
-  constructor(private readonly auth: Auth, private readonly errorService: ErrorService) {
-    if (this.auth) {
+
+  constructor(private readonly auth: Auth) {
+    if(this.auth) {
       this.user = authState(this.auth);
-      onAuthStateChanged(this.auth, (user: User | null) => {
+      onAuthStateChanged(this.auth, (user) => {
         this.user = of(user);
-        console.log("USER : ", user);
       },
-      (error: Error) => {
-      console.log("ERROR : ", error);
-      }
-    );
+      (error) => {
+        // TODO : Catch error in signin and signup function
+        console.error(error);
+      });
     }
   }
+
 
   public async signIn(email: string, password: string): Promise<UserCredential | null> {
     try {
       return await signInWithEmailAndPassword(this.auth, email, password);
-    } catch (error: unknown) {
-      console.log(error);
-      return null;
+    } catch (error) {
+      console.error(error);
     }
+    return null;
   }
 
-  public async signUp(email: string, password: string, firstname: string, lastname: string): Promise<UserCredential | null>
-  {
-    try {
-      const data: UserCredential = await createUserWithEmailAndPassword(this.auth, email, password);
-      const displayName = `${lastname.toLocaleUpperCase()} ${firstname}`;
-      await updateProfile(data.user, { displayName });
-      return data;
-    } catch (error) {
-      return null;
-    }
-  }
+  public async signUp(email: string, password: string, firstname: string,
+    lastname: string, identifiant: string, userService: UserService): Promise<UserCredential | null> {
 
-  public async signOut(): Promise<void> {
-    try {
-      await signOut(this.auth);
-    } catch (error) {
-      console.log(error);
-    }
+      try {
+        const data: UserCredential = await createUserWithEmailAndPassword(this.auth, email, password);
+        const displayName: string = firstname + ' ' + lastname;
+
+        await updateProfile(data.user, {displayName: displayName});
+
+        //TODO : ajouter l'user dans firebase
+        console.log(data);
+
+        return data;
+      } catch (error) {
+        console.error(error);
+      }
+
+      return null;
   }
 
 }
-
