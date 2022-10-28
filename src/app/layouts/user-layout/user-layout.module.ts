598ede534ed7d0efa25b32cb12b4a1f7774128f3
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2022-10-28 17:16:01 +0200

    user pages

--- src/app/layouts/user-layout/user-layout.module.ts
+++ src/app/layouts/user-layout/user-layout.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { NavbarComponent } from './navbar/navbar.component';
 import { UserLayoutComponent } from './user-layout.component';
 
 @NgModule({
-  declarations: [UserLayoutComponent],
+  declarations: [UserLayoutComponent,NavbarComponent],
   imports: [
     SharedModule
   ],
