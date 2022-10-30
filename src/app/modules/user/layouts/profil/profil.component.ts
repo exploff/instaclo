e740109a0777e228a0fbde66987b33e0f925f4de
Julien Bedia <bedia.pro@gmail.com>
2022-10-30 18:16:33 +0100

    Initialisation Profil component

--- src/app/modules/user/layouts/profil/profil.component.ts
+++ src/app/modules/user/layouts/profil/profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/core/services/authentification/authentification.service';
 
 @Component({
   selector: 'app-profil',
@@ -8,6 +9,5 @@ import { Component, OnInit } from '@angular/core';
 export class ProfilComponent {
 
   constructor() { }
-
 
 }
