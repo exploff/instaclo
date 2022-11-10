2c826fffd07278301c3f8bb9127dd80b5e905089
Nicolas NGUYEN <nngvk@studec.fr>
2022-11-10 15:10:05 +0100

    commit error nedd refactor

---
+++ src/app/modules/user/layouts/not-found-user/not-found-user.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NotFoundUserComponent } from './not-found-user.component';
+
+describe('NotFoundUserComponent', () => {
+  let component: NotFoundUserComponent;
+  let fixture: ComponentFixture<NotFoundUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NotFoundUserComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(NotFoundUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/modules/user/layouts/not-found-user/not-found-user.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found-user',
+  templateUrl: './not-found-user.component.html',
+  styleUrls: ['./not-found-user.component.scss']
+})
+export class NotFoundUserComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+
+    setTimeout( () => {
+      this.router.navigate(['/user']);
+    },
+    3000)
+  }
+
+}
