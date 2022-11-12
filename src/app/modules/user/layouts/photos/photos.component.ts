1cdab94f5120364962c2cf7110b7ac855b67925f
Hossameddine-khamma <66746696+Hossameddine-khamma@users.noreply.github.com>
2022-11-12 11:16:57 +0100

    camera
    init

--- src/app/modules/user/layouts/photos/photos.component.ts
+++ src/app/modules/user/layouts/photos/photos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-photos',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PhotosComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild ("videoPlayer") videoplayer !: ElementRef
+
+  public photo : SafeUrl = ""
+
+  private facingMode : "user" | "environment" = "user"
+
+  private stream : MediaStream | undefined
+
+  constructor(private sanitizer : DomSanitizer) { }
 
   ngOnInit() {
+    this.start()
+  }
+
+  public async takePhoto(){
+    if(this.stream){
+      const track : MediaStreamTrack = this.stream.getVideoTracks()[0]
+      const imageCapture : ImageCapture = new ImageCapture(track)
+      const photo : Blob = await imageCapture.takePhoto()
+      const objectURL = URL.createObjectURL(photo)
+      this.photo = this.sanitizer.bypassSecurityTrustUrl(objectURL)
+    }
+  }
+   public async switchCamera():Promise<void>{
+    if(await this.hasMultipleCamera()){
+      this.facingMode = this.facingMode === "user" ? "environment": "user"
+    }
+    this.stream?.getTracks()?.forEach( (track)=>{
+      track.stop()
+    } )
+    this.start()
+  }
+
+  public async start():Promise<void> {
+    try {
+      this.stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: this.facingMode }})
+      this.videoplayer.nativeElement.srcObject = this.stream;
+      this.videoplayer.nativeElement.play()
+    } catch (error) {
+      alert(error)
+    }
+  }
+
+  public stop():void{
+    this.stream?.getTracks()?.forEach((track) =>{
+      track.stop();
+    })
+  }
+
+  public async hasMultipleCamera():Promise<boolean>{
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    return devices.filter((d) => d.kind === "videoinput" ).length > 1
   }
 
 }
